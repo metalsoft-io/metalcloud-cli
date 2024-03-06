c3b5888138908f6ec6097a2570a0c1ddb09da188
Ron <ron@xron.net>
2024-03-06 12:23:16 +0200

    fixed merge conflict

--- pkg/ostemplate/cmd_os_template.go
+++ pkg/ostemplate/cmd_os_template.go
@@ -373,8 +373,10 @@ var OsTemplatesCmds = []command.Command{
 		ExecuteFunc: templateRegisterCmd,
 		Endpoint:    configuration.ExtendedEndpoint,
 		Example: `
-metalcloud-cli os-template register --name test-template --source-template Ubuntu/20.04/oob-uefi-boot --source-iso ubuntu-20.04.4-live-server-amd64.iso —-assets-update user-data:./replace_asset_1,vendor-data:./replace_asset_2
-metalcloud-cli os-template register --name test-100 --source-template Ubuntu/20.04/oob-uefi-boot --source-iso ubuntu-20.04.4-live-server-amd64.iso --replace-if-exists --skip-host-key-checking=false
+Utilizing the resources from repository: https://github.com/metalsoft-io/os-templates
+metalcloud-cli os-template register --name "Ubuntu 22.04 OOB adv" --source-template Ubuntu/22.04/oob-adv-uefi-boot --source-iso ubuntu-22.04.4-live-server-amd64.iso --replace-if-exists --skip-host-key-checking=false
+metalcloud-cli os-template register --name ubuntu-20.04-oob --source-template Ubuntu/20.04/oob-uefi-boot --source-iso ubuntu-20.04.4-live-server-amd64.iso --replace-if-exists --skip-host-key-checking=false
+metalcloud-cli os-template register --name ubuntu-20.04-oob --source-template Ubuntu/20.04/oob-uefi-boot --source-iso ubuntu-20.04.4-live-server-amd64.iso --assets-update user-data:./replace_asset_1,vendor-data:./replace_asset_2
 metalcloud-cli os-template register --name "SONiC Enterprise 4.0.2 - Spine" --source-template "SONiC/4.0.2/sonic-enterprise-premium-spine"
 `,
 	},
