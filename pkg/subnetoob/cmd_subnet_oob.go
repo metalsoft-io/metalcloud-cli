882730bbcfa29e601a615c106b88440ff3aef25e
alexcorman <alex.corman94@gmail.com>
2024-04-23 17:02:43 +0300

    subnet oob functions

--- pkg/subnetoob/cmd_subnet_oob.go
+++ pkg/subnetoob/cmd_subnet_oob.go
@@ -43,7 +43,6 @@ var SubnetOOBCmds = []command.Command{
 			c.Arguments = map[string]interface{}{
 				"subnet_oob_id": c.FlagSet.Int("id", command.NilDefaultInt, colors.Red("(Required)")+" Subnet oob's id"),
 				"format":        c.FlagSet.String("format", "yaml", "The output format. Supported values are 'json','csv','yaml'. The default format is human readable."),
-				"raw":           c.FlagSet.Bool("raw", false, colors.Green("(Flag)")+" When set the return will be a full dump of the object. This is useful when copying configurations. Only works with json and yaml formats."),
 			}
 		},
 		ExecuteFunc: subnetOOBGetCmd,
@@ -60,7 +59,6 @@ var SubnetOOBCmds = []command.Command{
 			c.Arguments = map[string]interface{}{
 				"format":                c.FlagSet.String("format", "yaml", "The input format. Supported values are 'json','yaml'. The default format is json."),
 				"read_config_from_file": c.FlagSet.String("f", command.NilDefaultStr, colors.Red("(Required)")+" Read configuration from file"),
-				"read_config_from_pipe": c.FlagSet.Bool("pipe", false, colors.Green("(Flag)")+" If set, read configuration from pipe instead of from a file. Either this flag or the -f option must be used."),
 				"return_id":             c.FlagSet.Bool("return-id", false, "Will print the ID of the created object. Useful for automating tasks."),
 			}
 		},
@@ -171,119 +169,45 @@ func subnetOOBListCmd(c *command.Command, client metalcloud.MetalCloudClient) (s
 }
 
 func subnetOOBGetCmd(c *command.Command, client metalcloud.MetalCloudClient) (string, error) {
-
 	id, ok := command.GetIntParamOk(c.Arguments["subnet_oob_id"])
 	if !ok {
 		return "", fmt.Errorf("-id is required")
 	}
 
-	s, err := client.SubnetOOBGet(id)
+	subnetOOB, err := client.SubnetOOBGet(id)
 	if err != nil {
 		return "", err
 	}
 
-	schema := []tableformatter.SchemaField{
-		{
-			FieldName: "ID",
-			FieldType: tableformatter.TypeInt,
-			FieldSize: 6,
-		},
-		{
-			FieldName: "LABEL",
-			FieldType: tableformatter.TypeString,
-			FieldSize: 6,
-		},
-		{
-			FieldName: "DATACENTER",
-			FieldType: tableformatter.TypeString,
-			FieldSize: 6,
-		},
-		{
-			FieldName: "PREFIX",
-			FieldType: tableformatter.TypeInt,
-			FieldSize: 2,
-		},
-		{
-			FieldName: "FOR",
-			FieldType: tableformatter.TypeString,
-			FieldSize: 5,
-		},
-		{
-			FieldName: "AUTOMATIC_ALLOC_ENABLED",
-			FieldType: tableformatter.TypeBool,
-			FieldSize: 8,
-		},
-		{
-			FieldName: "RANGE_START",
-			FieldType: tableformatter.TypeString,
-			FieldSize: 5,
-		},
-		{
-			FieldName: "RANGE_END",
-			FieldType: tableformatter.TypeString,
-			FieldSize: 5,
-		},
-	}
-
-	data := [][]interface{}{{
-		s.SubnetOOBID,
-		s.SubnetOOBLabel,
-		s.DatacenterName,
-		s.SubnetOOBPrefixSize,
-		s.SubnetOOBAllocateForResourceType,
-		s.SubnetOOBUseForAutoAllocation,
-		s.SubnetOOBRangeStartHumanReadable,
-		s.SubnetOOBRangeEndHumanReadable,
-	}}
-
-	var sb strings.Builder
-
 	format := command.GetStringParam(c.Arguments["format"])
-
-	if command.GetBoolParam(c.Arguments["raw"]) {
-		ret, err := objects.RenderRawObject(*s, format, "SubnetOOB")
-		if err != nil {
-			return "", err
-		}
-		sb.WriteString(ret)
-	} else {
-		table := tableformatter.Table{
-			Data:   data,
-			Schema: schema,
-		}
-		ret, err := table.RenderTransposedTable("subnet oob", "", format)
-		if err != nil {
-			return "", err
-		}
-		sb.WriteString(ret)
+	ret, err := objects.RenderRawObject(*subnetOOB, format, "SubnetOOB")
+	if err != nil {
+		return "", err
 	}
 
-	return sb.String(), nil
+	return ret, nil
 }
 
 func subnetOOBCreateCmd(c *command.Command, client metalcloud.MetalCloudClient) (string, error) {
-
-	var sn metalcloud.SubnetOOB
-
-	err := command.GetRawObjectFromCommand(c, &sn)
+	obj, err := objects.ReadSingleObjectFromCommand(c, client)
 	if err != nil {
 		return "", err
 	}
+	subnetOOB := (*obj).(metalcloud.SubnetOOB)
 
-	ret, err := client.SubnetOOBCreate(sn)
+	createdSubnetOOB, err := client.SubnetOOBCreate(subnetOOB)
 	if err != nil {
 		return "", err
 	}
 
 	if command.GetBoolParam(c.Arguments["return_id"]) {
-		return fmt.Sprintf("%d", ret.SubnetOOBID), nil
+		return fmt.Sprintf("%d", createdSubnetOOB.SubnetOOBID), nil
 	}
 
 	return "", err
 }
 
 func subnetOOBDeleteCmd(c *command.Command, client metalcloud.MetalCloudClient) (string, error) {
-
 	id, ok := command.GetIntParamOk(c.Arguments["subnet_oob_id"])
 	if !ok {
 		return "", fmt.Errorf("-id is required")
