4a41ea00aa8a644bfb8a4d3f88b15885a412a697
Mike Robski <mike.robski@metalsoft.io>
2025-04-09 18:41:16 +0300

    Added user management commands

--- cmd/metalcloud-cli/cmd/user.go
+++ cmd/metalcloud-cli/cmd/user.go
@@ -3,10 +3,18 @@ package cmd
 import (
 	"github.com/metalsoft-io/metalcloud-cli/cmd/metalcloud-cli/system"
 	"github.com/metalsoft-io/metalcloud-cli/internal/user"
+	"github.com/metalsoft-io/metalcloud-cli/pkg/utils"
 	"github.com/spf13/cobra"
 )
 
 var (
+	userFlags = struct {
+		configSource  string
+		accountId     float32
+		sshKeyContent string
+		reason        string
+	}{}
+
 	userCmd = &cobra.Command{
 		Use:     "user [command]",
 		Aliases: []string{"users"},
@@ -48,6 +56,205 @@ var (
 			return user.GetLimits(cmd.Context(), args[0])
 		},
 	}
+
+	userCreateCmd = &cobra.Command{
+		Use:          "create",
+		Aliases:      []string{"new"},
+		Short:        "Create a new user.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.USERS_AND_PERMISSIONS_WRITE},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			config, err := utils.ReadConfigFromPipeOrFile(userFlags.configSource)
+			if err != nil {
+				return err
+			}
+
+			return user.Create(cmd.Context(), config)
+		},
+	}
+
+	userArchiveCmd = &cobra.Command{
+		Use:          "archive user_id",
+		Aliases:      []string{"remove"},
+		Short:        "Archive a user.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.USERS_AND_PERMISSIONS_WRITE},
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return user.Archive(cmd.Context(), args[0])
+		},
+	}
+
+	userUnarchiveCmd = &cobra.Command{
+		Use:          "unarchive user_id",
+		Aliases:      []string{"restore"},
+		Short:        "Unarchive a user.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.USERS_AND_PERMISSIONS_WRITE},
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return user.Unarchive(cmd.Context(), args[0])
+		},
+	}
+
+	userLimitsUpdateCmd = &cobra.Command{
+		Use:          "limits-update user_id",
+		Aliases:      []string{"update-limits"},
+		Short:        "Update user limits.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.USERS_AND_PERMISSIONS_WRITE},
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			config, err := utils.ReadConfigFromPipeOrFile(userFlags.configSource)
+			if err != nil {
+				return err
+			}
+
+			return user.UpdateLimits(cmd.Context(), args[0], config)
+		},
+	}
+
+	userConfigUpdateCmd = &cobra.Command{
+		Use:          "config-update user_id",
+		Aliases:      []string{"update-config"},
+		Short:        "Update user configuration.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.USERS_AND_PERMISSIONS_WRITE},
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			config, err := utils.ReadConfigFromPipeOrFile(userFlags.configSource)
+			if err != nil {
+				return err
+			}
+
+			return user.UpdateConfig(cmd.Context(), args[0], config)
+		},
+	}
+
+	userChangeAccountCmd = &cobra.Command{
+		Use:          "change-account user_id",
+		Aliases:      []string{"move-account"},
+		Short:        "Change user account.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.USERS_AND_PERMISSIONS_WRITE},
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return user.ChangeAccount(cmd.Context(), args[0], userFlags.accountId)
+		},
+	}
+
+	userSshKeysGetCmd = &cobra.Command{
+		Use:          "ssh-keys user_id",
+		Aliases:      []string{"get-ssh-keys"},
+		Short:        "Get user SSH keys.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.USERS_AND_PERMISSIONS_READ},
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return user.GetSSHKeys(cmd.Context(), args[0])
+		},
+	}
+
+	userSshKeyAddCmd = &cobra.Command{
+		Use:          "ssh-key-add user_id",
+		Aliases:      []string{"add-ssh-key"},
+		Short:        "Add an SSH key to a user.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.USERS_AND_PERMISSIONS_WRITE},
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return user.AddSSHKey(cmd.Context(), args[0], userFlags.sshKeyContent)
+		},
+	}
+
+	userSshKeyDeleteCmd = &cobra.Command{
+		Use:          "ssh-key-delete user_id key_id",
+		Aliases:      []string{"delete-ssh-key", "remove-ssh-key"},
+		Short:        "Delete an SSH key from a user.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.USERS_AND_PERMISSIONS_WRITE},
+		Args:         cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return user.DeleteSSHKey(cmd.Context(), args[0], args[1])
+		},
+	}
+
+	userApiKeyGetCmd = &cobra.Command{
+		Use:          "api-key user_id",
+		Aliases:      []string{"get-api-key"},
+		Short:        "Get user API key.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.USERS_AND_PERMISSIONS_READ},
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return user.GetAPIKey(cmd.Context(), args[0])
+		},
+	}
+
+	userApiKeyRegenerateCmd = &cobra.Command{
+		Use:          "regenerate-api-key user_id",
+		Aliases:      []string{"new-api-key"},
+		Short:        "Regenerate user API key.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.USERS_AND_PERMISSIONS_WRITE},
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return user.RegenerateAPIKey(cmd.Context(), args[0])
+		},
+	}
+
+	userSuspendCmd = &cobra.Command{
+		Use:          "suspend user_id",
+		Aliases:      []string{"disable"},
+		Short:        "Suspend a user.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.USERS_AND_PERMISSIONS_WRITE},
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return user.Suspend(cmd.Context(), args[0], userFlags.reason)
+		},
+	}
+
+	userUnsuspendCmd = &cobra.Command{
+		Use:          "unsuspend user_id",
+		Aliases:      []string{"enable"},
+		Short:        "Unsuspend a user.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.USERS_AND_PERMISSIONS_WRITE},
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return user.Unsuspend(cmd.Context(), args[0])
+		},
+	}
+
+	userPermissionsGetCmd = &cobra.Command{
+		Use:          "permissions user_id",
+		Aliases:      []string{"get-permissions"},
+		Short:        "Get user permissions.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.USERS_AND_PERMISSIONS_READ},
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return user.GetPermissions(cmd.Context(), args[0])
+		},
+	}
+
+	userPermissionsUpdateCmd = &cobra.Command{
+		Use:          "permissions-update user_id",
+		Aliases:      []string{"update-permissions"},
+		Short:        "Update user permissions.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.USERS_AND_PERMISSIONS_WRITE},
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			config, err := utils.ReadConfigFromPipeOrFile(userFlags.configSource)
+			if err != nil {
+				return err
+			}
+
+			return user.UpdatePermissions(cmd.Context(), args[0], config)
+		},
+	}
 )
 
 func init() {
@@ -56,4 +263,51 @@ func init() {
 	userCmd.AddCommand(userListCmd)
 	userCmd.AddCommand(userGetCmd)
 	userCmd.AddCommand(userLimitsGetCmd)
+
+	// User create
+	userCmd.AddCommand(userCreateCmd)
+	userCreateCmd.Flags().StringVar(&userFlags.configSource, "config-source", "", "Source of the new user configuration. Can be 'pipe' or path to a JSON file.")
+	userCreateCmd.MarkFlagsOneRequired("config-source")
+
+	// User archive/unarchive
+	userCmd.AddCommand(userArchiveCmd)
+	userCmd.AddCommand(userUnarchiveCmd)
+
+	// User limits update
+	userCmd.AddCommand(userLimitsUpdateCmd)
+	userLimitsUpdateCmd.Flags().StringVar(&userFlags.configSource, "config-source", "", "Source of the user limits configuration. Can be 'pipe' or path to a JSON file.")
+	userLimitsUpdateCmd.MarkFlagsOneRequired("config-source")
+
+	// User config update
+	userCmd.AddCommand(userConfigUpdateCmd)
+	userConfigUpdateCmd.Flags().StringVar(&userFlags.configSource, "config-source", "", "Source of the user configuration. Can be 'pipe' or path to a JSON file.")
+	userConfigUpdateCmd.MarkFlagsOneRequired("config-source")
+
+	// Change account
+	userCmd.AddCommand(userChangeAccountCmd)
+	userChangeAccountCmd.Flags().Float32Var(&userFlags.accountId, "account-id", 0, "The ID of the account to move the user to.")
+	userChangeAccountCmd.MarkFlagRequired("account-id")
+
+	// SSH Keys
+	userCmd.AddCommand(userSshKeysGetCmd)
+	userCmd.AddCommand(userSshKeyAddCmd)
+	userSshKeyAddCmd.Flags().StringVar(&userFlags.sshKeyContent, "key", "", "The content of the SSH key.")
+	userSshKeyAddCmd.MarkFlagRequired("key")
+	userCmd.AddCommand(userSshKeyDeleteCmd)
+
+	// API Key
+	userCmd.AddCommand(userApiKeyGetCmd)
+	userCmd.AddCommand(userApiKeyRegenerateCmd)
+
+	// Suspend/Unsuspend
+	userCmd.AddCommand(userSuspendCmd)
+	userSuspendCmd.Flags().StringVar(&userFlags.reason, "reason", "", "The reason for suspending the user.")
+	userSuspendCmd.MarkFlagRequired("reason")
+	userCmd.AddCommand(userUnsuspendCmd)
+
+	// Permissions
+	userCmd.AddCommand(userPermissionsGetCmd)
+	userCmd.AddCommand(userPermissionsUpdateCmd)
+	userPermissionsUpdateCmd.Flags().StringVar(&userFlags.configSource, "config-source", "", "Source of the user permissions configuration. Can be 'pipe' or path to a JSON file.")
+	userPermissionsUpdateCmd.MarkFlagsOneRequired("config-source")
 }
--- internal/user/user.go
+++ internal/user/user.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strconv"
 
@@ -9,6 +10,7 @@ import (
 	"github.com/metalsoft-io/metalcloud-cli/pkg/formatter"
 	"github.com/metalsoft-io/metalcloud-cli/pkg/logger"
 	"github.com/metalsoft-io/metalcloud-cli/pkg/response_inspector"
+	sdk "github.com/metalsoft-io/metalcloud-sdk-go"
 )
 
 var userPrintConfig = formatter.PrintConfig{
@@ -42,6 +44,75 @@ var userPrintConfig = formatter.PrintConfig{
 	},
 }
 
+var userLimitsPrintConfig = formatter.PrintConfig{
+	FieldsConfig: map[string]formatter.RecordFieldConfig{
+		"ComputeNodesInstancesToProvisionLimit": {
+			Title: "Compute Nodes Limit",
+			Order: 1,
+		},
+		"DrivesAttachedToInstancesLimit": {
+			Title: "Drives Limit",
+			Order: 2,
+		},
+		"InfrastructuresLimit": {
+			Title: "Infrastructures Limit",
+			Order: 3,
+		},
+	},
+}
+
+var userApiKeyPrintConfig = formatter.PrintConfig{
+	FieldsConfig: map[string]formatter.RecordFieldConfig{
+		"ApiKey": {
+			Title: "API Key",
+			Order: 1,
+		},
+	},
+}
+
+var userSshKeysPrintConfig = formatter.PrintConfig{
+	FieldsConfig: map[string]formatter.RecordFieldConfig{
+		"Id": {
+			Title: "#",
+			Order: 1,
+		},
+		"Name": {
+			Title: "Name",
+			Order: 2,
+		},
+		"Fingerprint": {
+			Title: "Fingerprint",
+			Order: 3,
+		},
+		"CreatedTimestamp": {
+			Title:       "Created",
+			Transformer: formatter.FormatDateTimeValue,
+			Order:       4,
+		},
+	},
+}
+
+var userPermissionsPrintConfig = formatter.PrintConfig{
+	FieldsConfig: map[string]formatter.RecordFieldConfig{
+		"UserId": {
+			Title: "User ID",
+			Order: 1,
+		},
+		"ResourceType": {
+			Title: "Resource Type",
+			Order: 2,
+		},
+		"ResourceId": {
+			Title: "Resource ID",
+			Order: 3,
+		},
+		"PermissionLevel": {
+			Title: "Permission Level",
+			Order: 4,
+		},
+	},
+}
+
 func List(ctx context.Context) error {
 	logger.Get().Info().Msgf("Listing all users")
 
@@ -58,16 +129,14 @@ func List(ctx context.Context) error {
 func Get(ctx context.Context, userId string) error {
 	logger.Get().Info().Msgf("Get user '%s'", userId)
 
-	userIdNumber, err := strconv.ParseFloat(userId, 32)
+	userIdNumber, err := getUserId(userId)
 	if err != nil {
-		err := fmt.Errorf("invalid user ID: '%s'", userId)
-		logger.Get().Error().Err(err).Msg("")
 		return err
 	}
 
 	client := api.GetApiClient(ctx)
 
-	userInfo, httpRes, err := client.UserAPI.GetUser(ctx, float32(userIdNumber)).Execute()
+	userInfo, httpRes, err := client.UserAPI.GetUser(ctx, userIdNumber).Execute()
 	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
 		return err
 	}
@@ -78,19 +147,370 @@ func Get(ctx context.Context, userId string) error {
 func GetLimits(ctx context.Context, userId string) error {
 	logger.Get().Info().Msgf("Get user '%s' limits", userId)
 
-	userIdNumber, err := strconv.ParseFloat(userId, 32)
+	userIdNumber, err := getUserId(userId)
 	if err != nil {
-		err := fmt.Errorf("invalid user ID: '%s'", userId)
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	userLimits, httpRes, err := client.UserAPI.GetUserLimits(ctx, userIdNumber).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	return formatter.PrintResult(userLimits, &userLimitsPrintConfig)
+}
+
+func Create(ctx context.Context, config []byte) error {
+	logger.Get().Info().Msgf("Creating user")
+
+	var userConfig sdk.CreateUser
+	err := json.Unmarshal(config, &userConfig)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	userInfo, httpRes, err := client.UserAPI.CreateUserAuthorized(ctx).CreateUser(userConfig).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	return formatter.PrintResult(userInfo, &userPrintConfig)
+}
+
+func Archive(ctx context.Context, userId string) error {
+	logger.Get().Info().Msgf("Archiving user '%s'", userId)
+
+	userIdNumber, revision, err := getUserIdAndRevision(ctx, userId)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	userInfo, httpRes, err := client.UserAPI.ArchiveUser(ctx, userIdNumber).IfMatch(revision).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	logger.Get().Info().Msgf("User '%s' archived", userId)
+	return formatter.PrintResult(userInfo, &userPrintConfig)
+}
+
+func Unarchive(ctx context.Context, userId string) error {
+	logger.Get().Info().Msgf("Unarchiving user '%s'", userId)
+
+	userIdNumber, revision, err := getUserIdAndRevision(ctx, userId)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	userInfo, httpRes, err := client.UserAPI.UnarchiveUser(ctx, userIdNumber).IfMatch(revision).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	logger.Get().Info().Msgf("User '%s' unarchived", userId)
+	return formatter.PrintResult(userInfo, &userPrintConfig)
+}
+
+func UpdateLimits(ctx context.Context, userId string, config []byte) error {
+	logger.Get().Info().Msgf("Updating limits for user '%s'", userId)
+
+	userIdNumber, revision, err := getUserIdAndRevision(ctx, userId)
+	if err != nil {
+		return err
+	}
+
+	var userLimitsConfig sdk.UserLimits
+	err = json.Unmarshal(config, &userLimitsConfig)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	userLimits, httpRes, err := client.UserAPI.UpdateUserLimits(ctx, userIdNumber).UserLimits(userLimitsConfig).IfMatch(revision).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	logger.Get().Info().Msgf("Limits updated for user '%s'", userId)
+	return formatter.PrintResult(userLimits, &userLimitsPrintConfig)
+}
+
+func UpdateConfig(ctx context.Context, userId string, config []byte) error {
+	logger.Get().Info().Msgf("Updating configuration for user '%s'", userId)
+
+	userIdNumber, revision, err := getUserIdAndRevision(ctx, userId)
+	if err != nil {
+		return err
+	}
+
+	var userConfig sdk.UpdateUser
+	err = json.Unmarshal(config, &userConfig)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	userConfiguration, httpRes, err := client.UserAPI.UpdateUserConfig(ctx, userIdNumber).UpdateUser(userConfig).IfMatch(revision).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	logger.Get().Info().Msgf("Configuration updated for user '%s'", userId)
+	return formatter.PrintResult(userConfiguration, nil)
+}
+
+func ChangeAccount(ctx context.Context, userId string, accountId float32) error {
+	logger.Get().Info().Msgf("Changing account for user '%s' to account '%g'", userId, accountId)
+
+	userIdNumber, revision, err := getUserIdAndRevision(ctx, userId)
+	if err != nil {
+		return err
+	}
+
+	changeAccount := sdk.ChangeUserAccount{
+		NewAccountId: accountId,
+	}
+
+	client := api.GetApiClient(ctx)
+
+	userInfo, httpRes, err := client.UserAPI.ChangeUserAccount(ctx, userIdNumber).ChangeUserAccount(changeAccount).IfMatch(revision).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	logger.Get().Info().Msgf("Account changed for user '%s' to account '%g'", userId, accountId)
+	return formatter.PrintResult(userInfo, &userPrintConfig)
+}
+
+func GetSSHKeys(ctx context.Context, userId string) error {
+	logger.Get().Info().Msgf("Getting SSH keys for user '%s'", userId)
+
+	userIdNumber, err := getUserId(userId)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	sshKeys, httpRes, err := client.UserAPI.GetUserSshKeys(ctx, userIdNumber).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	return formatter.PrintResult(sshKeys, &userSshKeysPrintConfig)
+}
+
+func AddSSHKey(ctx context.Context, userId string, keyContent string) error {
+	logger.Get().Info().Msgf("Adding SSH key for user '%s'", userId)
+
+	userIdNumber, err := getUserId(userId)
+	if err != nil {
+		return err
+	}
+
+	sshKeyData := sdk.CreateUserSSHKeyDto{
+		SshKey: keyContent,
+	}
+
+	client := api.GetApiClient(ctx)
+
+	sshKey, httpRes, err := client.UserAPI.AddUserSshKey(ctx, userIdNumber).CreateUserSSHKeyDto(sshKeyData).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	logger.Get().Info().Msgf("SSH key added for user '%s'", userId)
+	return formatter.PrintResult(sshKey, &userSshKeysPrintConfig)
+}
+
+func DeleteSSHKey(ctx context.Context, userId string, keyId string) error {
+	logger.Get().Info().Msgf("Deleting SSH key '%s' for user '%s'", keyId, userId)
+
+	userIdNumber, err := getUserId(userId)
+	if err != nil {
+		return err
+	}
+
+	keyIdNumber, err := strconv.ParseFloat(keyId, 32)
+	if err != nil {
+		err := fmt.Errorf("invalid SSH key ID: '%s'", keyId)
 		logger.Get().Error().Err(err).Msg("")
 		return err
 	}
 
 	client := api.GetApiClient(ctx)
 
-	userInfo, httpRes, err := client.UserAPI.GetUserLimits(ctx, float32(userIdNumber)).Execute()
+	httpRes, err := client.UserAPI.DeleteUserSshKey(ctx, userIdNumber, float32(keyIdNumber)).Execute()
 	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
 		return err
 	}
 
-	return formatter.PrintResult(userInfo, &userPrintConfig)
+	logger.Get().Info().Msgf("SSH key '%s' deleted for user '%s'", keyId, userId)
+	return nil
+}
+
+func GetAPIKey(ctx context.Context, userId string) error {
+	logger.Get().Info().Msgf("Getting API key for user '%s'", userId)
+
+	userIdNumber, err := getUserId(userId)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	apiKey, httpRes, err := client.UserAPI.GetUserApiKey(ctx, userIdNumber).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	return formatter.PrintResult(apiKey, &userApiKeyPrintConfig)
+}
+
+func RegenerateAPIKey(ctx context.Context, userId string) error {
+	logger.Get().Info().Msgf("Regenerating API key for user '%s'", userId)
+
+	userIdNumber, revision, err := getUserIdAndRevision(ctx, userId)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	_, httpRes, err := client.UserAPI.RegenerateUserApiKey(ctx, userIdNumber).IfMatch(revision).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	logger.Get().Info().Msgf("API key regenerated for user '%s'", userId)
+
+	// Get the new API key to display it
+	apiKey, httpRes, err := client.UserAPI.GetUserApiKey(ctx, userIdNumber).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	return formatter.PrintResult(apiKey, &userApiKeyPrintConfig)
+}
+
+func Suspend(ctx context.Context, userId string, reason string) error {
+	logger.Get().Info().Msgf("Suspending user '%s'", userId)
+
+	userIdNumber, revision, err := getUserIdAndRevision(ctx, userId)
+	if err != nil {
+		return err
+	}
+
+	suspendReason := sdk.UserSuspend{
+		SuspendReason: reason,
+	}
+
+	client := api.GetApiClient(ctx)
+
+	suspendInfo, httpRes, err := client.UserAPI.SuspendUser(ctx, userIdNumber).UserSuspend(suspendReason).IfMatch(revision).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	logger.Get().Info().Msgf("User '%s' suspended", userId)
+	return formatter.PrintResult(suspendInfo, nil)
+}
+
+func Unsuspend(ctx context.Context, userId string) error {
+	logger.Get().Info().Msgf("Unsuspending user '%s'", userId)
+
+	userIdNumber, revision, err := getUserIdAndRevision(ctx, userId)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	httpRes, err := client.UserAPI.UnsuspendUser(ctx, userIdNumber).IfMatch(revision).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	logger.Get().Info().Msgf("User '%s' unsuspended", userId)
+	return nil
+}
+
+func GetPermissions(ctx context.Context, userId string) error {
+	logger.Get().Info().Msgf("Getting permissions for user '%s'", userId)
+
+	userIdNumber, err := getUserId(userId)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	permissions, httpRes, err := client.UserAPI.GetUserPermissions(ctx, userIdNumber).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	return formatter.PrintResult(permissions, &userPermissionsPrintConfig)
+}
+
+func UpdatePermissions(ctx context.Context, userId string, config []byte) error {
+	logger.Get().Info().Msgf("Updating permissions for user '%s'", userId)
+
+	userIdNumber, revision, err := getUserIdAndRevision(ctx, userId)
+	if err != nil {
+		return err
+	}
+
+	var permissionsConfig sdk.UpdateUserPermissionsDto
+	err = json.Unmarshal(config, &permissionsConfig)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	permissions, httpRes, err := client.UserAPI.UpdateUserPermissions(ctx, userIdNumber).UpdateUserPermissionsDto(permissionsConfig).IfMatch(revision).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	logger.Get().Info().Msgf("Permissions updated for user '%s'", userId)
+	return formatter.PrintResult(permissions, &userPermissionsPrintConfig)
+}
+
+func getUserId(userId string) (float32, error) {
+	userIdNumeric, err := strconv.ParseFloat(userId, 32)
+	if err != nil {
+		err := fmt.Errorf("invalid user ID: '%s'", userId)
+		logger.Get().Error().Err(err).Msg("")
+		return 0, err
+	}
+
+	return float32(userIdNumeric), nil
+}
+
+func getUserIdAndRevision(ctx context.Context, userId string) (float32, string, error) {
+	userIdNumeric, err := getUserId(userId)
+	if err != nil {
+		return 0, "", err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	user, httpRes, err := client.UserAPI.GetUser(ctx, float32(userIdNumeric)).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return 0, "", err
+	}
+
+	return float32(userIdNumeric), strconv.Itoa(int(user.Revision)), nil
 }
