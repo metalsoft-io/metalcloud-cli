a5531c142d8d4c74eac2db7e646125c70e4c2a1a
Mike Robski <mike.robski@metalsoft.io>
2025-04-11 10:09:34 +0300

    Added drive CLI commands

---
+++ cmd/metalcloud-cli/cmd/drive.go
@@ -0,0 +1,181 @@
+package cmd
+
+import (
+	"github.com/metalsoft-io/metalcloud-cli/cmd/metalcloud-cli/system"
+	"github.com/metalsoft-io/metalcloud-cli/internal/drive"
+	"github.com/metalsoft-io/metalcloud-cli/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+var (
+	driveFlags = struct {
+		configSource string
+		filterStatus string
+	}{}
+
+	driveCmd = &cobra.Command{
+		Use:     "drive [command]",
+		Aliases: []string{"drives", "disk"},
+		Short:   "Drive management",
+		Long:    `Drive management commands.`,
+	}
+
+	driveListCmd = &cobra.Command{
+		Use:          "list infrastructure_id_or_label",
+		Aliases:      []string{"ls"},
+		Short:        "List all drives for an infrastructure.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.STORAGE_READ},
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return drive.DriveList(cmd.Context(), args[0], driveFlags.filterStatus)
+		},
+	}
+
+	driveGetCmd = &cobra.Command{
+		Use:          "get infrastructure_id_or_label drive_id",
+		Aliases:      []string{"show"},
+		Short:        "Get drive details.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.STORAGE_READ},
+		Args:         cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return drive.DriveGet(cmd.Context(), args[0], args[1])
+		},
+	}
+
+	driveCreateCmd = &cobra.Command{
+		Use:          "create infrastructure_id_or_label",
+		Aliases:      []string{"new"},
+		Short:        "Create a new drive.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.STORAGE_READ},
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			config, err := utils.ReadConfigFromPipeOrFile(driveFlags.configSource)
+			if err != nil {
+				return err
+			}
+
+			return drive.DriveCreate(cmd.Context(), args[0], config)
+		},
+	}
+
+	driveDeleteCmd = &cobra.Command{
+		Use:          "delete infrastructure_id_or_label drive_id",
+		Aliases:      []string{"rm"},
+		Short:        "Delete a drive.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.STORAGE_READ},
+		Args:         cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return drive.DriveDelete(cmd.Context(), args[0], args[1])
+		},
+	}
+
+	driveUpdateConfigCmd = &cobra.Command{
+		Use:          "update-config infrastructure_id_or_label drive_id",
+		Aliases:      []string{"config-update"},
+		Short:        "Update drive configuration.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.STORAGE_READ},
+		Args:         cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			config, err := utils.ReadConfigFromPipeOrFile(driveFlags.configSource)
+			if err != nil {
+				return err
+			}
+
+			return drive.DriveUpdateConfig(cmd.Context(), args[0], args[1], config)
+		},
+	}
+
+	driveUpdateMetaCmd = &cobra.Command{
+		Use:          "update-meta infrastructure_id_or_label drive_id",
+		Aliases:      []string{"meta-update"},
+		Short:        "Update drive metadata.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.STORAGE_READ},
+		Args:         cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			config, err := utils.ReadConfigFromPipeOrFile(driveFlags.configSource)
+			if err != nil {
+				return err
+			}
+
+			return drive.DriveUpdateMeta(cmd.Context(), args[0], args[1], config)
+		},
+	}
+
+	driveGetHostsCmd = &cobra.Command{
+		Use:          "get-hosts infrastructure_id_or_label drive_id",
+		Aliases:      []string{"hosts"},
+		Short:        "Get hosts for a drive.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.STORAGE_READ},
+		Args:         cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return drive.DriveGetHosts(cmd.Context(), args[0], args[1])
+		},
+	}
+
+	driveUpdateHostsCmd = &cobra.Command{
+		Use:          "update-hosts infrastructure_id_or_label drive_id",
+		Aliases:      []string{"hosts-update"},
+		Short:        "Update hosts for a drive.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.STORAGE_READ},
+		Args:         cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			config, err := utils.ReadConfigFromPipeOrFile(driveFlags.configSource)
+			if err != nil {
+				return err
+			}
+
+			return drive.DriveUpdateHosts(cmd.Context(), args[0], args[1], config)
+		},
+	}
+
+	driveGetConfigInfoCmd = &cobra.Command{
+		Use:          "config-info infrastructure_id_or_label drive_id",
+		Aliases:      []string{"get-config-info"},
+		Short:        "Get configuration information for a drive.",
+		SilenceUsage: true,
+		Annotations:  map[string]string{system.REQUIRED_PERMISSION: system.STORAGE_READ},
+		Args:         cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return drive.DriveGetConfigInfo(cmd.Context(), args[0], args[1])
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(driveCmd)
+
+	driveCmd.AddCommand(driveListCmd)
+	driveListCmd.Flags().StringVar(&driveFlags.filterStatus, "filter-status", "", "Filter the result by drive status.")
+
+	driveCmd.AddCommand(driveGetCmd)
+
+	driveCmd.AddCommand(driveCreateCmd)
+	driveCreateCmd.Flags().StringVar(&driveFlags.configSource, "config-source", "", "Source of the new drive configuration. Can be 'pipe' or path to a JSON file.")
+	driveCreateCmd.MarkFlagsOneRequired("config-source")
+
+	driveCmd.AddCommand(driveDeleteCmd)
+
+	driveCmd.AddCommand(driveUpdateConfigCmd)
+	driveUpdateConfigCmd.Flags().StringVar(&driveFlags.configSource, "config-source", "", "Source of the drive configuration updates. Can be 'pipe' or path to a JSON file.")
+	driveUpdateConfigCmd.MarkFlagsOneRequired("config-source")
+
+	driveCmd.AddCommand(driveUpdateMetaCmd)
+	driveUpdateMetaCmd.Flags().StringVar(&driveFlags.configSource, "config-source", "", "Source of the drive metadata updates. Can be 'pipe' or path to a JSON file.")
+	driveUpdateMetaCmd.MarkFlagsOneRequired("config-source")
+
+	driveCmd.AddCommand(driveGetHostsCmd)
+
+	driveCmd.AddCommand(driveUpdateHostsCmd)
+	driveUpdateHostsCmd.Flags().StringVar(&driveFlags.configSource, "config-source", "", "Source of the drive hosts configuration. Can be 'pipe' or path to a JSON file.")
+	driveUpdateHostsCmd.MarkFlagsOneRequired("config-source")
+
+	driveCmd.AddCommand(driveGetConfigInfoCmd)
+}
---
+++ internal/drive/drive.go
@@ -0,0 +1,406 @@
+package drive
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	"github.com/metalsoft-io/metalcloud-cli/internal/infrastructure"
+	"github.com/metalsoft-io/metalcloud-cli/pkg/api"
+	"github.com/metalsoft-io/metalcloud-cli/pkg/formatter"
+	"github.com/metalsoft-io/metalcloud-cli/pkg/logger"
+	"github.com/metalsoft-io/metalcloud-cli/pkg/response_inspector"
+	sdk "github.com/metalsoft-io/metalcloud-sdk-go"
+)
+
+var drivePrintConfig = formatter.PrintConfig{
+	FieldsConfig: map[string]formatter.RecordFieldConfig{
+		"Id": {
+			Title: "#",
+			Order: 1,
+		},
+		"Label": {
+			MaxWidth: 30,
+			Order:    2,
+		},
+		"SizeMBytes": {
+			Title: "Size (MB)",
+			Order: 3,
+		},
+		"StoragePoolId": {
+			Title: "Pool ID",
+			Order: 4,
+		},
+		"InfrastructureId": {
+			Title: "Infra ID",
+			Order: 5,
+		},
+		"ServiceStatus": {
+			Title:       "Status",
+			Transformer: formatter.FormatStatusValue,
+			Order:       6,
+		},
+		"Config": {
+			Hidden: true,
+			InnerFields: map[string]formatter.RecordFieldConfig{
+				"DeployStatus": {
+					Title:       "Deploy Status",
+					Transformer: formatter.FormatStatusValue,
+					Order:       7,
+				},
+				"DeployType": {
+					Title: "Deploy Type",
+					Order: 8,
+				},
+			},
+		},
+		"WWN": {
+			Title: "WWN",
+			Order: 9,
+		},
+	},
+}
+
+var driveHostsPrintConfig = formatter.PrintConfig{
+	FieldsConfig: map[string]formatter.RecordFieldConfig{
+		"ServerInstanceGroupId": {
+			Title: "Group ID",
+			Order: 1,
+		},
+		"ServerInstanceId": {
+			Title: "Server ID",
+			Order: 2,
+		},
+		"ServerId": {
+			Title: "HW Server ID",
+			Order: 3,
+		},
+		"Status": {
+			Title:       "Status",
+			Transformer: formatter.FormatStatusValue,
+			Order:       4,
+		},
+		"TapeEnabled": {
+			Title: "Tape Enabled",
+			Order: 5,
+		},
+	},
+}
+
+func DriveList(ctx context.Context, infrastructureIdOrLabel string, filterStatus string) error {
+	logger.Get().Info().Msgf("Listing drives for infrastructure '%s'", infrastructureIdOrLabel)
+
+	// Get the infrastructure ID from ID or label
+	infrastructureInfo, err := infrastructure.GetInfrastructureByIdOrLabel(ctx, infrastructureIdOrLabel)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	request := client.DriveAPI.GetInfrastructureDrives(ctx, infrastructureInfo.Id)
+
+	if filterStatus != "" {
+		request = request.FilterServiceStatus([]string{"$eq:" + filterStatus})
+	}
+
+	driveList, httpRes, err := request.Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	return formatter.PrintResult(driveList, &drivePrintConfig)
+}
+
+func DriveGet(ctx context.Context, infrastructureIdOrLabel string, driveId string) error {
+	logger.Get().Info().Msgf("Get drive '%s' details", driveId)
+
+	// Get the infrastructure ID from ID or label
+	infrastructureInfo, err := infrastructure.GetInfrastructureByIdOrLabel(ctx, infrastructureIdOrLabel)
+	if err != nil {
+		return err
+	}
+
+	driveIdNumeric, err := getDriveId(driveId)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	drive, httpRes, err := client.DriveAPI.GetInfrastructureDrive(ctx, infrastructureInfo.Id, driveIdNumeric).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	return formatter.PrintResult(drive, &drivePrintConfig)
+}
+
+func DriveCreate(ctx context.Context, infrastructureIdOrLabel string, config []byte) error {
+	logger.Get().Info().Msgf("Creating drive for infrastructure '%s'", infrastructureIdOrLabel)
+
+	// Get the infrastructure ID from ID or label
+	infrastructureInfo, err := infrastructure.GetInfrastructureByIdOrLabel(ctx, infrastructureIdOrLabel)
+	if err != nil {
+		return err
+	}
+
+	var driveConfig sdk.CreateDrive
+	err = json.Unmarshal(config, &driveConfig)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	drive, httpRes, err := client.DriveAPI.
+		CreateDrive(ctx, infrastructureInfo.Id).
+		CreateDrive(driveConfig).
+		Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	return formatter.PrintResult(drive, &drivePrintConfig)
+}
+
+func DriveDelete(ctx context.Context, infrastructureIdOrLabel string, driveId string) error {
+	logger.Get().Info().Msgf("Deleting drive '%s'", driveId)
+
+	// Get the infrastructure ID from ID or label
+	infrastructureInfo, err := infrastructure.GetInfrastructureByIdOrLabel(ctx, infrastructureIdOrLabel)
+	if err != nil {
+		return err
+	}
+
+	driveIdNumeric, revision, err := getDriveIdAndRevision(ctx, infrastructureInfo.Id, driveId)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	httpRes, err := client.DriveAPI.
+		DeleteDrive(ctx, infrastructureInfo.Id, driveIdNumeric).
+		IfMatch(revision).
+		Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	logger.Get().Info().Msgf("Drive '%s' deleted", driveId)
+	return nil
+}
+
+func DriveUpdateConfig(ctx context.Context, infrastructureIdOrLabel string, driveId string, config []byte) error {
+	logger.Get().Info().Msgf("Updating drive '%s' configuration", driveId)
+
+	// Get the infrastructure ID from ID or label
+	infrastructureInfo, err := infrastructure.GetInfrastructureByIdOrLabel(ctx, infrastructureIdOrLabel)
+	if err != nil {
+		return err
+	}
+
+	driveIdNumeric, revision, err := getDriveIdAndRevision(ctx, infrastructureInfo.Id, driveId)
+	if err != nil {
+		return err
+	}
+
+	var driveConfigUpdate sdk.UpdateDrive
+	err = json.Unmarshal(config, &driveConfigUpdate)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	drive, httpRes, err := client.DriveAPI.
+		PatchDriveConfig(ctx, infrastructureInfo.Id, driveIdNumeric).
+		UpdateDrive(driveConfigUpdate).
+		IfMatch(revision).
+		Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	return formatter.PrintResult(drive, &drivePrintConfig)
+}
+
+func DriveUpdateMeta(ctx context.Context, infrastructureIdOrLabel string, driveId string, config []byte) error {
+	logger.Get().Info().Msgf("Updating drive '%s' metadata", driveId)
+
+	// Get the infrastructure ID from ID or label
+	infrastructureInfo, err := infrastructure.GetInfrastructureByIdOrLabel(ctx, infrastructureIdOrLabel)
+	if err != nil {
+		return err
+	}
+
+	driveIdNumeric, err := getDriveId(driveId)
+	if err != nil {
+		return err
+	}
+
+	var driveMetaUpdate sdk.UpdateDriveMeta
+	err = json.Unmarshal(config, &driveMetaUpdate)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	drive, httpRes, err := client.DriveAPI.
+		PatchDriveMeta(ctx, infrastructureInfo.Id, driveIdNumeric).
+		UpdateDriveMeta(driveMetaUpdate).
+		Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	return formatter.PrintResult(drive, &drivePrintConfig)
+}
+
+func DriveGetHosts(ctx context.Context, infrastructureIdOrLabel string, driveId string) error {
+	logger.Get().Info().Msgf("Getting hosts for drive '%s'", driveId)
+
+	// Get the infrastructure ID from ID or label
+	infrastructureInfo, err := infrastructure.GetInfrastructureByIdOrLabel(ctx, infrastructureIdOrLabel)
+	if err != nil {
+		return err
+	}
+
+	driveIdNumeric, err := getDriveId(driveId)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	hosts, httpRes, err := client.DriveAPI.
+		GetDriveHosts(ctx, infrastructureInfo.Id, driveIdNumeric).
+		Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	return formatter.PrintResult(hosts, &driveHostsPrintConfig)
+}
+
+func DriveUpdateHosts(ctx context.Context, infrastructureIdOrLabel string, driveId string, config []byte) error {
+	logger.Get().Info().Msgf("Updating hosts for drive '%s'", driveId)
+
+	// Get the infrastructure ID from ID or label
+	infrastructureInfo, err := infrastructure.GetInfrastructureByIdOrLabel(ctx, infrastructureIdOrLabel)
+	if err != nil {
+		return err
+	}
+
+	driveIdNumeric, err := getDriveId(driveId)
+	if err != nil {
+		return err
+	}
+
+	var hostsUpdate sdk.DriveHostsModifyBulk
+	err = json.Unmarshal(config, &hostsUpdate)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	hosts, httpRes, err := client.DriveAPI.
+		UpdateDriveServerInstanceGroupHostsBulk(ctx, infrastructureInfo.Id, driveIdNumeric).
+		DriveHostsModifyBulk(hostsUpdate).
+		Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	return formatter.PrintResult(hosts, &driveHostsPrintConfig)
+}
+
+func DriveGetConfigInfo(ctx context.Context, infrastructureIdOrLabel string, driveId string) error {
+	logger.Get().Info().Msgf("Getting configuration info for drive '%s'", driveId)
+
+	// Get the infrastructure ID from ID or label
+	infrastructureInfo, err := infrastructure.GetInfrastructureByIdOrLabel(ctx, infrastructureIdOrLabel)
+	if err != nil {
+		return err
+	}
+
+	driveIdNumeric, err := getDriveId(driveId)
+	if err != nil {
+		return err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	configInfo, httpRes, err := client.DriveAPI.
+		GetDriveConfigInfo(ctx, infrastructureInfo.Id, driveIdNumeric).
+		Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return err
+	}
+
+	return formatter.PrintResult(configInfo, &formatter.PrintConfig{
+		FieldsConfig: map[string]formatter.RecordFieldConfig{
+			"Label": {
+				Title: "Label",
+				Order: 1,
+			},
+			"SizeMBytes": {
+				Title: "Size (MB)",
+				Order: 2,
+			},
+			"DriveDeployId": {
+				Title: "Deploy ID",
+				Order: 3,
+			},
+			"DeployType": {
+				Title: "Deploy Type",
+				Order: 4,
+			},
+			"DeployStatus": {
+				Title:       "Deploy Status",
+				Transformer: formatter.FormatStatusValue,
+				Order:       5,
+			},
+			"LogicalNetworkId": {
+				Title: "Logical Network ID",
+				Order: 6,
+			},
+			"UpdatedTimestamp": {
+				Title:       "Updated",
+				Transformer: formatter.FormatDateTimeValue,
+				Order:       7,
+			},
+		},
+	})
+}
+
+func getDriveId(driveId string) (float32, error) {
+	driveIdNumeric, err := strconv.ParseFloat(driveId, 32)
+	if err != nil {
+		err := fmt.Errorf("invalid drive ID: '%s'", driveId)
+		logger.Get().Error().Err(err).Msg("")
+		return 0, err
+	}
+
+	return float32(driveIdNumeric), nil
+}
+
+func getDriveIdAndRevision(ctx context.Context, infrastructureId float32, driveId string) (float32, string, error) {
+	driveIdNumeric, err := getDriveId(driveId)
+	if err != nil {
+		return 0, "", err
+	}
+
+	client := api.GetApiClient(ctx)
+
+	drive, httpRes, err := client.DriveAPI.GetInfrastructureDrive(ctx, infrastructureId, float32(driveIdNumeric)).Execute()
+	if err := response_inspector.InspectResponse(httpRes, err); err != nil {
+		return 0, "", err
+	}
+
+	return float32(driveIdNumeric), strconv.Itoa(int(drive.Revision)), nil
+}
